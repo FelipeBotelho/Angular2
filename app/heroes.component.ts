f8740a171d6f612d884b57541b8dcc948afe7342
Felipe Botelho <felipe.botelho@programmers.com.br>
2017-01-04 15:06:40 -0200

    Construção de Modulos, faltando HTTP

---
+++ app/heroes.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
+
+@Component({
+  moduleId:module.id,
+  selector: 'my-heroes',
+  templateUrl: 'heroes.component.html',
+  styleUrls: ['heroes.component.css'],
+})
+export class HeroesComponent implements OnInit{
+  ngOnInit():void{
+    this.getHeroes();
+  }
+
+  constructor (private router:Router, private heroService: HeroService){}
+
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  getHeroes():void{
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+}
+
+
